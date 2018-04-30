55d4d5dff0ad76991c0163d545ca037070035b79
JP Gouigoux <jp.gouigoux@free.fr>
2018-04-30 22:08:40 +0200

    Adding bootstrap

--- mo-uxrequesttype/index.js
+++ mo-uxrequesttype/index.js
@@ -6,7 +6,24 @@ var urlapi = process.env.URLAPI || '/api/requesttypes';
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-	res.send('<html><head><title>MiddleOffice - Request types</title></head><body><form method="POST" action="' + urlapi + '"><input type="text" name="code"></input><button type="submit"></button></form></body></html>');
+    res.send('<html>'
+    +'<head>'
+    +'<title>MiddleOffice - Request types</title>'
+    +'<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">'
+    +'</head>'
+    +'<body>'
+    +'<form method="POST" action="' + urlapi + '">'
+    +'<div class="form-group">'
+    +'<input type="text" class="form-control input-lg" placeholder="Request type code" name="code"></input>'
+    +'<input type="text" class="form-control input-lg" placeholder="Label for vote #1" name="vote1"></input>'
+    +'<input type="text" class="form-control input-lg" placeholder="URL to call upon vote #1" name="url1"></input>'
+    +'<input type="text" class="form-control input-lg" placeholder="Label for vote #2" name="vote2"></input>'
+    +'<input type="text" class="form-control input-lg" placeholder="URL to call upon vote #2" name="url2"></input>'
+    +'<button type="submit">Enregistrer</button>'
+    +'</div>'
+    +'</form>'
+    +'</body>'
+    +'</html>');
 });
 
 app.listen(port);
