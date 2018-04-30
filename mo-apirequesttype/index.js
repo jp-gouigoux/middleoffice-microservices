e870b27b3e07421ec2ccbb459acf0fe3d0ccd50c
JP Gouigoux <jp.gouigoux@free.fr>
2018-04-30 23:15:11 +0200

    Adding some error management

--- mo-apirequesttype/index.js
+++ mo-apirequesttype/index.js
@@ -31,8 +31,16 @@ app.post('/api/requesttypes/', function(req, res) {
             console.log('Unable to connect to the MongoDB server: ', err);
         } else {
             console.log('Connected to MongoDB server');
-            var requesttype = { code: req.params.code, vote1: req.params.vote1, url1: req.params.url1, vote2: req.params.vote2, url2: req.params.url2 };
-            db.collection('requesttypes').insert(requesttype);
+            var requesttype = { code: req.body.code, vote1: req.body.vote1, url1: req.body.url1, vote2: req.body.vote2, url2: req.body.url2 };
+            db.collection('requesttypes').insertOne(requesttype, function(error, result) {
+                if (error) {
+                    console.log('Error in inserting request type into MongoDB collection: ', error);
+                } else {
+                    res.contentType('application/json');
+                    res.status(200);
+                    res.json(result);    
+                }
+            });
         }
     });  
 });
