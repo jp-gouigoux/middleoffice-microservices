65b26657d1a5a957c87fbc0613844e5a20887797
JP Gouigoux <jp.gouigoux@free.fr>
2018-05-01 18:29:10 +0200

    Missing prefix for ObjectId

--- mo-apirequest/index.js
+++ mo-apirequest/index.js
@@ -42,7 +42,7 @@ app.get('/api/requests/:id', function(req, res) {
             res.status(500).end();
         } else {
             console.log('Connected to MongoDB server');
-            db.collection('requests').findOne({ "_id": ObjectId(req.params.id) }, function (error, result) {
+            db.collection('requests').findOne({ "_id": mongodb.ObjectId(req.params.id) }, function (error, result) {
                 if (error) {
                     console.log('Unable to retrieve request ' + req.params.id + ' from MongoDB collection: ', error);
                     res.status(500).end();
