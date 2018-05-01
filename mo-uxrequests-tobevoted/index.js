e10896f44260624f47e96a45d22a7f4feb1fabbe
JP Gouigoux <jp.gouigoux@free.fr>
2018-05-01 15:07:18 +0200

    Looping over requests

--- mo-uxrequests-tobevoted/index.js
+++ mo-uxrequests-tobevoted/index.js
@@ -5,7 +5,7 @@ var base_url = process.env.BASE_URL || 'http://localhost/middleoffice';
 var apiux_url = base_url + '/api/ux/requests';
 
 app.get('*', function(req, res) {
-    console.log(req);
+    console.log(req); // TODO : If possible, retrieve APIUX_URL from current URL
     var content = '<html>'
         +'<head>'
         +'<title>MiddleOffice - Requests to be voted</title>'
@@ -14,11 +14,21 @@ app.get('*', function(req, res) {
         +'<body>'
         +'<h1>List of requests to be voted</h1>'
         +'<ul>';
-    content += '<li><a href="' + apiux_url + '/id/vote">Demande bidon</a></li>'
-    content += '</ul>'
-        +'</body>'
-        +'</html>';
-    res.send(content);
+    var requestslist_url = base_url + '/api/requests';
+    request(requestslist_url, { json: true}, function(error, result, body) {
+        if (error) {
+            console.log('Error while calling ' + requestslist_url + ': ', error);
+            res.status(500).end();
+        } else {
+            var liste = JSON.parse(body);
+            liste.array.forEach(element => {
+                content += '<li><a href="' + apiux_url + '/' + element._id + '/vote">Demande bidon</a></li>';
+            });
+            content += '</ul>'
+                +'</body>'
+                +'</html>';
+            res.send(content);
+        }});
 });
 
 app.listen(port);
